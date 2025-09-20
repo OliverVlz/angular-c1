28645cbe20c3e31a0d920e7c98329e278f738bca
Oliver Velasquez <odvelasquez@unillanos.edu.co>
2025-09-20 16:17:05 +0000

    ejercicio en clase 1.1 correcion

--- class1/src/main.ts
+++ class1/src/main.ts
@@ -4,6 +4,8 @@ import './ejercicios/ejercicio3';
 */
 import './ejercicios/ejercicio4';
 import './ejercicios/ejercicio5';
+import './ejercicios/ejercicio6';
+import './ejercicios/ejercicio7';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = `Hola Mundo!`;
