f48c4c044315df114b1d0c35232be3d4497d086f
Oliver Velasquez <odvelasquez@unillanos.edu.co>
2025-09-20 01:42:42 +0000

    feat: variables

---
+++ class1/src/ejercicios/ejercicio1.ts
@@ -0,0 +1,11 @@
+let nombre:string | number = 'Juan';
+nombre = 'prueba';
+
+const edad = 3;
+const edad1 = edad.toString();
+
+console.log(`Hola ${nombre}`);
+console.log(`La edad es ${edad1}`);
+console.log(typeof edad1);
+
+export {};
