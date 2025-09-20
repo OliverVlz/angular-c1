a670f2a2bcc881f58eea3dd7d6f69c4543fb4029
Oliver Velasquez <odvelasquez@unillanos.edu.co>
2025-09-20 16:17:36 +0000

    correcion a ejemplo clase

--- class1/src/main.ts
+++ class1/src/main.ts
@@ -6,6 +6,7 @@ import './ejercicios/ejercicio4';
 import './ejercicios/ejercicio5';
 import './ejercicios/ejercicio6';
 import './ejercicios/ejercicio7';
+import './ejercicios/ejercicio8';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = `Hola Mundo!`;
