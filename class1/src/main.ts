793b4d32a836320315cf5bbcf83214516646bd23
Oliver Velasquez <odvelasquez@unillanos.edu.co>
2025-09-20 01:42:13 +0000

    init

---
+++ class1/src/main.ts
@@ -0,0 +1,6 @@
+import './ejercicios/ejercicio1';
+import './ejercicios/ejercicio2';
+import './ejercicios/ejercicio3';
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app.innerHTML = `Hola Mundo!`;
