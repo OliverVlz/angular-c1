92c26ed1cb8b55e7b46ef00e3f82deaf1155e703
Oliver Velasquez <odvelasquez@unillanos.edu.co>
2025-10-04 16:11:23 +0000

    feat: class 04/10

---
+++ ServiciosApi/src/app/gifs/components/side-menu/side-menu.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SideMenuOptions } from './side-menu-options/side-menu-options';
+import { SideMenuHeader } from './side-menu-header/side-menu-header';
+
+@Component({
+  selector: 'gifs-side-menu',
+  imports: [SideMenuHeader, SideMenuOptions ],
+  templateUrl: './side-menu.html',
+  styles: ``
+})
+export class SideMenu {
+
+}
---
+++ ServiciosApi/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  companyName: 'Gifs Test',
+  companyName2: 'App',
+  companySlogan: 'Your favorite GIFs in one place'
+};
